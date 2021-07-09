8a6a6ed23a7a7b2342d240c9109b67a83aecb812
assaadha <hassan.assaad@uni-a.de>
2021-07-09 13:56:51 +0300

    export models and required files

---
+++ src/helpers/index.ts
@@ -0,0 +1,6 @@
+export * from './enums/aspects.enum';
+export * from './enums/mappings.enum';
+export * from './enums/specially-attributes.enum';
+export * from './enums/types.enum';
+export * from './key-value';
+export * from './utilities';
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 import * as validator from './validate';
 import * as fs from 'fs';
+export * from './models/index';
 
-const file = fs.readFileSync('D:\\CX-Typescript\\test\\DrugBank - Carrier drugs.cx', 'utf-8');
+const file = fs.readFileSync('D:\\CX-Typescript\\test\\networks\\Fanconi Anemia Gene Ontology (FangO).cx', 'utf-8');
 
-const result = validator.validateDataAgainstSchema(file);
+const result = validator.validateCxData(file);
 
 result.map((error) => {
     console.log(error.aspectName);
