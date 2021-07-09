670ed1cfe0414ae5c269e47d3e644dd2a1c78dfd
assaadha <hassan.assaad@uni-a.de>
2021-07-09 13:55:49 +0300

    move cyTableColumn into attributes schema

--- src/schema/attributes.schema.ts
+++ src/schema/attributes.schema.ts
@@ -122,6 +122,67 @@ export const _cyHiddenAttributesArr = {
   allOf: typeRules(AspectCytoscape.CY_HIDDEN_ATTRIBUTES),
 };
 
+
+/******************  Cy Table Column ******************/
+
+export const _cyTableColumn = {
+  type: 'object',
+  properties: {
+    s: {
+      type: 'integer',
+      minimum: 0,
+      errorMessage: {
+        type: i18.getErrorMessage('type_is_integer', AspectCytoscape.CY_TABLE_COLUMN, '${0#}'),
+        minimum: i18.getErrorMessage('minimum', AspectCytoscape.CY_TABLE_COLUMN, '${0#}'),
+      },
+    },
+    n: {
+      type: 'string',
+      errorMessage: {
+        type: i18.getErrorMessage('type_is_string', AspectCytoscape.CY_TABLE_COLUMN, '${0#}'),
+      },
+    },
+    d: {
+      enum: Utilities.DataTypes,
+      errorMessage: {
+        enum: i18.getErrorMessage('enum', AspectCytoscape.CY_TABLE_COLUMN, `[${Utilities.DataTypes.join(', ')}]`),
+      },
+    },
+    applies_to: {
+      enum: Utilities.TableColumnAppliesTo,
+      errorMessage: {
+        enum: i18.getErrorMessage(
+          'enum',
+          AspectCytoscape.CY_TABLE_COLUMN,
+          `[${Utilities.TableColumnAppliesTo.join(', ')}]`,
+        ),
+      },
+    },
+  },
+  required: ['n', 'applies_to'],
+  errorMessage: {
+    required: {
+      n: i18.getErrorMessage('required', AspectCytoscape.CY_TABLE_COLUMN, '"n"'),
+      applies_to: i18.getErrorMessage('required', AspectCytoscape.CY_TABLE_COLUMN, '"applies_to"'),
+    },
+  },
+  additionalProperties: {
+    not: true,
+    errorMessage: i18.getErrorMessage('additional_properties', AspectCytoscape.CY_TABLE_COLUMN, '${0#}'),
+  },
+};
+
+export const _cyTableColumnArr = {
+  type: 'array',
+  items: _cyTableColumn,
+  errorMessage: {
+    type: i18.getErrorMessage('type_is_array', AspectCytoscape.CY_TABLE_COLUMN, `"${AspectCytoscape.CY_TABLE_COLUMN}"`),
+  },
+  allOf: typeRules(AspectCytoscape.CY_TABLE_COLUMN),
+};
+
+
+
 /******************  Shared Attributes ******************/
 
 function typeRules(aspectType: AspectCore | AspectCytoscape) {
