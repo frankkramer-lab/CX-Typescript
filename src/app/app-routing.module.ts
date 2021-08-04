6a238fbd276adf2576cbff805d11aed161b897cc
assaadha <hassan.assaad@uni-a.de>
2021-08-04 22:02:47 +0300

    [WIP]: add angular routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,41 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DocumentationComponent } from './components/pages/documentation/documentation.component';
+import { EditComponent } from './components/pages/edit/edit.component';
+import { HomeComponent } from './components/pages/home/home.component';
+import { StatisticsComponent } from './components/pages/statistics/statistics.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent,
+    pathMatch: 'full',
+  },
+  {
+    path: 'doc',
+    component: DocumentationComponent,
+    pathMatch: 'full',
+  },
+  {
+    path: 'editor',
+    component: EditComponent,
+    pathMatch: 'full',
+  },
+  {
+    path: 'statistics',
+    component: StatisticsComponent,
+    pathMatch: 'full',
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  { path: '**', redirectTo: 'home', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,46 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-import { MonacoEditorModule } from '@materia-ui/ngx-monaco-editor';
-import { TreeviewModule } from 'ngx-treeview';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
-
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
+import { TabsModule } from 'ngx-bootstrap/tabs';
+import { AccordionModule } from 'ngx-bootstrap/accordion';
+import { MonacoEditorModule } from '@materia-ui/ngx-monaco-editor';
+import { GridsterModule } from 'angular-gridster2';
+
+import { HomeComponent } from './components/pages/home/home.component';
+import { DocumentationComponent } from './components/pages/documentation/documentation.component';
+import { StatisticsComponent } from './components/pages/statistics/statistics.component';
+import { EditComponent } from './components/pages/edit/edit.component';
+
 import { EditorComponent } from './components/controls/editor/editor.component';
 import { SideBarManageComponent } from './components/controls/side-bar-manage/side-bar-manage.component';
-import { TreeViewComponent } from './components/controls/tree-view/tree-view.component';
-import { MainComponent } from './components/main/main.component';
 import { NetworkListComponent } from './components/controls/network-list/network-list.component';
+import { NavbarComponent } from './components/controls/navbar/navbar.component';
+import { SideBarTabsComponent } from './components/controls/side-bar-tabs/side-bar-tabs.component';
+import { NetworkInformationComponent } from './components/controls/network-information/network-information.component';
+import { ErrorListComponent } from './components/controls/error-list/error-list.component';
+import { NetworkStatisticsComponent } from './components/controls/network-statistics/network-statistics.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     EditorComponent,
-    TreeViewComponent,
     SideBarManageComponent,
+    SideBarTabsComponent,
     NetworkListComponent,
-    MainComponent,
+    NavbarComponent,
+    ErrorListComponent,
+    NetworkInformationComponent,
+    NetworkStatisticsComponent,
+
+    HomeComponent,
+    DocumentationComponent,
+    StatisticsComponent,
+    EditComponent,
   ],
   imports: [
     BrowserModule,
@@ -28,7 +48,12 @@ import { NetworkListComponent } from './components/controls/network-list/network
     FormsModule,
     AppRoutingModule,
     MonacoEditorModule,
-    TreeviewModule.forRoot(),
+    GridsterModule,
+
+    BrowserAnimationsModule,
+    TooltipModule.forRoot(),
+    TabsModule.forRoot(),
+    AccordionModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent],
