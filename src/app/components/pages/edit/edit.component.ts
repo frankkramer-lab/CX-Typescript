ffb0654ca38bf46d5ed6271114808287e1717811
assaadha <hassan.assaad@uni-a.de>
2021-09-14 15:11:50 +0300

    [BugFix]: fix gridster options

--- src/app/components/pages/edit/edit.component.ts
+++ src/app/components/pages/edit/edit.component.ts
@@ -3,6 +3,8 @@ import {
   CompactType,
   DisplayGrid,
   GridsterConfig,
+  GridsterItem,
+  GridsterItemComponentInterface,
   GridType,
 } from 'angular-gridster2';
 
@@ -14,7 +16,7 @@ import {
 export class EditComponent implements OnInit {
   options: GridsterConfig = {
     gridType: GridType.Fit,
-    compactType: CompactType.CompactLeftAndUp,
+    compactType: CompactType.None,
     margin: 4,
     outerMargin: false,
     outerMarginTop: null,
@@ -22,12 +24,21 @@ export class EditComponent implements OnInit {
     outerMarginBottom: null,
     outerMarginLeft: null,
     useTransformPositioning: true,
-    mobileBreakpoint: 640,
-    maxCols: 4,
+    minCols: 1,
+    maxCols: 100,
     minRows: 1,
     maxRows: 1,
+    maxItemCols: 100,
+    minItemCols: 1,
+    maxItemRows: 100,
+    minItemRows: 1,
+    maxItemArea: 2500,
+    minItemArea: 1,
+    defaultItemCols: 1,
+    defaultItemRows: 1,
+    fixedColWidth: 50,
     draggable: {
-      enabled: true,
+      enabled: false,
     },
     resizable: {
       enabled: true,
@@ -36,18 +47,39 @@ export class EditComponent implements OnInit {
     pushItems: true,
     disablePushOnDrag: false,
     disablePushOnResize: false,
-    pushDirections: { north: false, east: true, south: false, west: true },
-    pushResizeItems: false,
-    displayGrid: DisplayGrid.None,
+    pushDirections: { north: true, east: true, south: true, west: true },
+    pushResizeItems: true,
+    displayGrid: DisplayGrid.OnDragAndResize,
+    // itemResizeCallback: this.itemResize.bind(this),
     disableWindowResize: false,
     disableWarnings: false,
     scrollToNewItems: false,
   };
 
-  sideBarTabs = { cols: 1, rows: 1, y: 0, x: 0 };
-  editor = { cols: 2, rows: 1, y: 0, x: 1 };
+  sideBarTabs = {
+    cols: 2,
+    rows: 1,
+    y: 0,
+    x: 0,
+    resizeEnabled: true,
+    compactEnabled: false,
+  };
+
+  editor = {
+    cols: 5,
+    rows: 1,
+    y: 0,
+    x: 1,
+    resizeEnabled: true,
+    compactEnabled: true,
+  };
 
   constructor() {}
 
   ngOnInit() {}
+
+  itemResize(
+    item: GridsterItem,
+    itemComponent: GridsterItemComponentInterface
+  ) {}
 }
