8b5fb385eb3c04ac9ee4b01739dcbfc061a33357
assaadha <hassan.assaad@uni-a.de>
2021-09-14 15:09:02 +0300

    [WIP]: add tabs to edit page

--- src/app/components/controls/side-bar-tabs/side-bar-tabs.component.ts
+++ src/app/components/controls/side-bar-tabs/side-bar-tabs.component.ts
@@ -1,23 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { TabsetComponent } from 'ngx-bootstrap/tabs';
 
 @Component({
   selector: 'app-side-bar-tabs',
   templateUrl: './side-bar-tabs.component.html',
-  styleUrls: ['./side-bar-tabs.component.scss']
+  styleUrls: ['./side-bar-tabs.component.scss'],
 })
 export class SideBarTabsComponent implements OnInit {
-
   errorLength: number = 0;
-  constructor() { }
+
+  @ViewChild('tabset', { static: true })
+  tabsets: TabsetComponent;
+
+  constructor(private route: ActivatedRoute) {}
 
   ngOnInit() {
+    this.route.queryParams.subscribe((params) => {
+      const selectedTab = params.tab;
+      if (selectedTab === 'upload') {
+        this.tabsets.tabs[0].active = true;
+      } else if (selectedTab === 'info') {
+        this.tabsets.tabs[1].active = true;
+      } else if (selectedTab === 'statistics') {
+        this.tabsets.tabs[3].active = true;
+      }
+    });
   }
 
   getErrorsLength(event: any) {
-    console.log(event);
-    if(event !== undefined) {
-      this.errorLength = event
+    if (event !== undefined) {
+      this.errorLength = event;
     }
   }
-
 }
